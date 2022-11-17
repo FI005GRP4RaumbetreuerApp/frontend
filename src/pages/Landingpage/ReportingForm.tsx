9ee0f47437ec599b70ed302bb7c6073a4255d7cd
sabdulla <s.abdulla@neusta.de>
2022-11-17 16:21:37 +0100

    send data from reporting form to server

--- src/pages/Landingpage/ReportingForm.tsx
+++ src/pages/Landingpage/ReportingForm.tsx
@@ -5,6 +5,8 @@ import { useHistory } from 'react-router-dom'
 import useAddReport from "../../api/useAddReport";
 import { AddReport } from "../../types/AddReport";
 import Input from '../../components/Input'
+import { useGetRooms } from "../../api";
+import { Room } from "../../types";
 
 interface Props{
   selectRoom: string
@@ -34,8 +36,11 @@ export const ReportingForm: FC<Props> = ({selectRoom}) => {
   const [status, setStatus] = useState<string>('')
 
   const [errorMessage, setErrorMessage] = useState<string>('')
-
-
+  const [rooms, setRooms] = useState()
+  const getRooms = useGetRooms()
+  getRooms({accessToken: cookies.access_token}).then((res: Room[]) =>{
+    setRooms(res)
+  });
   const addReportForm = async (
     accessToken: string,
     meldungs_typ: string,
@@ -90,7 +95,7 @@ export const ReportingForm: FC<Props> = ({selectRoom}) => {
           <div className="h-full min-h-96">
             <Dialog.Panel className="w-full h-fit flex flex-col max-w-2xl bg-white rounded-2xl">
               <Dialog.Title className="flex justify-center items-center bg-primary text-white font-bold h-16 rounded-t-2xl">
-                Defekt für Raum [FI005] melden
+                Defekt für Raum {raumId} melden
               </Dialog.Title>
               { errorMessage.length > 0 ? <div className="px-5 py-3 text-red-700 bg-red-100">{errorMessage}</div> : null}
 
@@ -110,6 +115,24 @@ export const ReportingForm: FC<Props> = ({selectRoom}) => {
                   placeHolder="Raumname"
                   onChange={(value: string) => setraumId(value)}
                 />
+                <div className="relative">
+                  <Listbox value={status} onChange={setStatus}>
+                    <Listbox.Button
+                      className="text-left bg-backgroundGray px-5 py-2 rounded-2xl w-96 placeholder-neutral-600 text-neutral-600 focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary"
+                    >{status ? meldungsStatus.find(s => s.key === status).label : 'Auswahl'}</Listbox.Button>
+                    <Listbox.Options className="w-full absolute top-10 left-0 z-50 bg-white p-3 rounded-xl shadow flex flex-col gap-2 divide-y">
+                      {meldungsStatus.map((status) => (
+                        <Listbox.Option
+                          key={status.id}
+                          value={status.key}
+                          className="cursor-pointer hover:bg-secondary px-2"
+                        >
+                          {status.label}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Listbox>
+                </div>
                 <div className="relative">
                   <Listbox value={status} onChange={setStatus}>
                     <Listbox.Button
