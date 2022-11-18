b1b26c23ae94a8ac7c5c329cd8d9bc48ef134aa1
sabdulla <s.abdulla@neusta.de>
2022-11-18 11:04:41 +0100

    fixed layout space

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 
 const Footer: FC = () => {
   return (
-    <div className="flex flex-col h-12 bg-black self-bottom text-xs justify-center items-center mt-auto">
+    <div className="flex flex-col h-12 bg-black absolute inset-x-0 bottom-0 text-xs justify-center items-center">
       <span className="text-white">©Georg-Simon-Ohm Berufskolleg 2022</span>
     </div>
   )
--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -11,7 +11,7 @@ export const PageLayout: FC<PageLayoutProps> = ({
   showHeaderButtons = false,
 }) => {
   return (
-    <div className={`flex flex-col min-h-screen`}>
+    <div className={`flex flex-col h-screen`}>
       <Header showHeaderButtons={showHeaderButtons} />
       <div className="flex flex-col w-full bg-backgroundGray justify-center items-center">
         {children}
